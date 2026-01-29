0cb4ee89d8d12f937d0605cdc94433e561565ccf
SantiagoBA13 <santiago.arriola.15@gmail.com>
2026-01-29 10:05:54 -0600

    Create capacitor.config.ts

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.lumus.musicplayer',
+  appName: 'LUMUS Music Player',
+  webDir: 'web',
+  bundledWebRuntime: false
+};
+
+export default config;
